1aa65e6da5719115a1be3b74a4c7bed3e158cb5b
Ignacio Payeras <ignacio.payeras@alumnos.ucn.cl>
2021-05-09 13:19:21 -0400

    Collider Slingshots

--- pinball/Assets/Scripts/Flippers.cs
+++ pinball/Assets/Scripts/Flippers.cs
@@ -4,27 +4,27 @@ using UnityEngine;
 
 public class Flippers : MonoBehaviour
 {
-    public float restPosition = 0f;
-    public float pressedPosition = 45f;
-    public float hitStrenght = 10000f;
-    public float flipperDamper = 150f;
-    HingeJoint hinge;
-    public string inputName;
+    public float restPosition = 0f; //Posicion de reposo
+    public float pressedPosition = 45f; //Cuanto se demora en llegar a su posicion inicial
+    public float hitStrenght = 10000f; //Fuerza que ejerce
+    public float flipperDamper = 150f; //Fuerza con que se devuelve el flipper
+    HingeJoint hinge; 
+    public string inputName; //Nombre de la funcion
     // Start is called before the first frame update
     void Start()
     {
-        hinge = GetComponent<HingeJoint>();
-        hinge.useSpring = true;
+        hinge = GetComponent<HingeJoint>(); //Obtener objeto de rotacion
+        hinge.useSpring = true; //Inicializa con true
     }
 
     // Update is called once per frame
     void Update()
     {
-        JointSpring spring = new JointSpring();
+        JointSpring spring = new JointSpring(); 
         spring.spring = hitStrenght;
         spring.damper = flipperDamper;
-        if(Input.GetAxis(inputName) == 1){
-            spring.targetPosition = pressedPosition;
+        if(Input.GetAxis(inputName) == 1){ //Si se aprieta el boton seleccionado
+            spring.targetPosition = pressedPosition; 
         }
         else{
             spring.targetPosition = restPosition;
