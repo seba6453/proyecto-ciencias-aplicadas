5d3238315324de95fe1bf89ed477ce3963553900
PePe-14 <73206031+PePe-14@users.noreply.github.com>
2021-05-09 17:00:00 -0400

    Caida DT

--- pinball/Assets/Scripts/ScriptDropT.cs
+++ pinball/Assets/Scripts/ScriptDropT.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class ScriptDropT : MonoBehaviour
 {
-    public GameObject dropT;
-    private Collision collision;
+    Collider col;
+    Rigidbody rb; 
+    void Start(){
+        col = GetComponent<Collider>(); //Se obtiene el Collider de 1 DropTarget
+        rb = GetComponent<Rigidbody>(); //Se obtiene el Rigidbody de 1 DropTarget
+    }
 
-    void OnCollisionEnter(Collision collision){
-        if(collision.gameObject.name == "Sphere"){
-            collision
+    void OnCollisionEnter(Collision collision) //collision ->DT ;collision.collider -> Pelota
+    {
+        if(collision.collider.gameObject.CompareTag("Player")){ //Si el objecto que toco el DT tiene tag "Player" entra
+            col.enabled = false;                        // El collider del DT se desactiva y el DT se cae
+            rb.AddForce(Vector3.down *100f, ForceMode.Impulse);  //Hace que caiga con mayor impulso
         }
     }
-    void Update{
-        
 
-    }
 }
