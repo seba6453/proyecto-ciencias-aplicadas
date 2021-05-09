0f605ace4b7f211ea292e786674489f8bb53d2b1
PePe-14 <73206031+PePe-14@users.noreply.github.com>
2021-05-09 01:06:57 -0400

    Wire y drops
    Falta terminar el code del collider del drop targets

---
+++ pinball/Assets/Scripts/ScriptDropT.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptDropT : MonoBehaviour
+{
+    public GameObject dropT;
+    private Collision collision;
+
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.name == "Sphere"){
+            collision
+        }
+    }
+    void Update{
+        
+
+    }
+}
