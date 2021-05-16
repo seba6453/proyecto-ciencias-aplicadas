04b4e4a4742382e606ec9eca17dec62f2a56cfc2
PePe-14 <73206031+PePe-14@users.noreply.github.com>
2021-05-16 02:17:08 -0400

    DropTargets Hechos
    Falta q suban con mas rapidez

---
+++ pinball/Assets/Scripts/Colision.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colision : MonoBehaviour
+{
+    Collider col;
+    Rigidbody rb;
+    void Start(){
+        col = GetComponent<Collider>();
+        rb = GetComponent<Rigidbody>();
+    }
+    public void OnCollisionEnter(Collision collision) //collision ->DT ;collision.collider -> Pelota
+    {
+        if(collision.collider.gameObject.CompareTag("Ball")){ //Si el objecto que toco el DT tiene tag "Ball" entra
+            Destroy(collision.collider.gameObject);
+            col.enabled = false;                        // El collider del DT se desactiva y el DT se cae
+            rb.AddForce(Vector3.down * 100f, ForceMode.Impulse);  //Hace que caiga con mayor impulso
+            ScriptDropT.cantDT -=1;
+            Debug.Log(ScriptDropT.cantDT);
+        }
+    }
+}
--- pinball/Assets/Scripts/ScriptDropT.cs
+++ pinball/Assets/Scripts/ScriptDropT.cs
@@ -4,36 +4,51 @@ using UnityEngine;
 
 public class ScriptDropT : MonoBehaviour
 {
-    Collider col;
-    Rigidbody rb; 
+    Collider col1;
+    Rigidbody rb1;
+    Collider col2;
+    Rigidbody rb2;
+    Collider col3;
+    Rigidbody rb3; 
     public static int cantDT = 0;
+    public GameObject DT1;
+    public GameObject DT2;
+    public GameObject DT3; 
     
     void Start(){
-        col = GetComponent<Collider>(); //Se obtiene el Collider de 1 DropTarget
-        rb = GetComponent<Rigidbody>(); //Se obtiene el Rigidbody de 1 DropTarget
-        cantDT += 1;
-        Debug.Log(cantDT);
+        col1 = GetCollider(DT1); 
+        rb1 = GetRigidbody(DT1); 
+        col2 = GetCollider(DT2); 
+        rb2 = GetRigidbody(DT2);
+        col3 = GetCollider(DT3); 
+        rb3 = GetRigidbody(DT3);
     }
 
-    public void OnCollisionEnter(Collision collision) //collision ->DT ;collision.collider -> Pelota
-    {
-        if(collision.collider.gameObject.CompareTag("Ball")){ //Si el objecto que toco el DT tiene tag "Ball" entra
-            col.enabled = false;                        // El collider del DT se desactiva y el DT se cae
-            rb.AddForce(Vector3.down * 100f, ForceMode.Impulse);  //Hace que caiga con mayor impulso
-            cantDT -= 1;
-            Debug.Log(cantDT);
-        }
+    public Collider GetCollider(GameObject gameObject){
+        Collider col = gameObject.GetComponent<Collider>(); //Se obtiene el Collider de 1 DropTarget
+        return col;
+    }
+
+    public Rigidbody GetRigidbody(GameObject gameObject){
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>(); //Se obtiene el Rigidbody de 1 DropTarget
+        cantDT += 1;
+        Debug.Log(cantDT);
+        return rb;
     }
 
     void Update()
     {
         if(cantDT == 0){
-            rb.AddForce(Vector3.up * 100f, ForceMode.Impulse);  //Hace que suban con mayor impulso
-            col.enabled = true;
-            cantDT += 1;
-            Debug.Log(cantDT);    
-           
+            subir(col1,rb1);
+            subir(col2,rb2);
+            subir(col3,rb3);   
         }
     }
 
+    void subir(Collider col,Rigidbody rb){
+        rb.AddForce(Vector3.up * 100f, ForceMode.Impulse);  //Hace que suban con mayor impulso
+        cantDT += 1;
+        Debug.Log(cantDT);
+        col.enabled = true;
+    }
 }
