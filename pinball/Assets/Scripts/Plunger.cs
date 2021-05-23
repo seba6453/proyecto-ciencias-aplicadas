8360716d5607a53d0d28caa1d66170e2cfd27657
Ignacio Payeras <ignacio.payeras@alumnos.ucn.cl>
2021-05-22 21:45:27 -0400

    xD

--- pinball/Assets/Scripts/Plunger.cs
+++ pinball/Assets/Scripts/Plunger.cs
@@ -9,12 +9,24 @@ public class Plunger : MonoBehaviour
     public Slider powerSlider;
     List<Rigidbody> ballList;
     bool ballReady;
+    private Rigidbody rigidBody;    
+	private Vector3 position;
+    private Vector3 originalPosition;
+    private Vector3 finalPosition;
+ 	// private bool push;
+	private GameObject ball;
     // Start is called before the first frame update
     void Start()
     {
         powerSlider.minValue = 0f;
         powerSlider.maxValue = maxPower;
         ballList = new List<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody>();    
+    	position = transform.position;
+        originalPosition = new Vector3(gameObject.transform.position.x,gameObject.transform.position.y,gameObject.transform.position.z);
+    	finalPosition = new Vector3(gameObject.transform.position.x,gameObject.transform.position.y,gameObject.transform.position.z + maxPower);
+        
+        ball = GameObject.FindWithTag("Ball");
     }
 
     // Update is called once per frame
@@ -30,14 +42,23 @@ public class Plunger : MonoBehaviour
         if(ballList.Count > 0){
             ballReady = true;
             if(Input.GetKey(KeyCode.Space)){
-                if(power <= maxPower){
+                if(power < maxPower){
                     power += 50 * Time.deltaTime;
+                    position.z -= 0.05f;
+                    position.y -= 0.05f; 
+                    rigidBody.transform.position = position;
+                        
+                }else if(power == maxPower){
+                    power += 50 * Time.deltaTime;
+                    rigidBody.transform.position = finalPosition;
                 }
+                
             }
             if(Input.GetKeyUp(KeyCode.Space)){
                 foreach(Rigidbody r in ballList){
                     r.AddForce(power * Vector3.forward);
                 }
+                //position = originalPosition;
             }
         }
         else{
